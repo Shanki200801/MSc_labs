bcb91db18a9b20ea1ca4952858fa41a3b3ccffa4
Shanki200801 <shashank200801@gmail.com>
2022-09-13 10:38:22 +0530

    lab 13th sept

---
+++ DAA labs/DataStructures/intQueue.java
@@ -0,0 +1,79 @@
+package DataStructures;
+import java.util.*;
+
+public class intQueue {
+    
+    int front = -1;
+    int rear = -1;
+    int[] array;
+    intQueue(int size){
+         this.array = new int[size];    
+    }
+    intQueue(){
+         this.array = new int[10];
+    }
+    
+    
+    public int deQueue(){
+        if(isQempty()){
+            System.out.println("Queue is empty and cant delete");
+            return -1;
+        }
+        int t = array[front];
+        if(front==rear){
+            front=rear=-1;
+            return t;
+        }      
+        front++;
+        return t;
+    }
+
+    public void enQueue(int ele){
+        
+        if(isQfull()){
+            System.out.println("Queue is full");
+            return;
+        }
+        if(front==-1){
+            front=0;    
+        }
+        rear++;
+        array[rear]= ele;
+    } 
+
+    public boolean isQempty(){
+        if(front==-1)return true;
+        return false;
+    }
+
+    public boolean isQfull(){
+        if(front==0&&rear==array.length-1)
+            return true;
+        return false;
+    }
+
+    public int peek(){
+        return array[front];
+    }
+
+    public void display(){
+        for(int i=front;i<=rear;i++){
+            System.out.print(array[i]+" ");
+        }
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        
+        intQueue newQueue = new intQueue(5);
+
+        newQueue.enQueue(5);
+        newQueue.enQueue(9);
+        newQueue.enQueue(4);
+        newQueue.enQueue(29);      
+        newQueue.enQueue(2);
+        newQueue.enQueue(6);
+        newQueue.deQueue();
+        newQueue.display();
+    }
+}
