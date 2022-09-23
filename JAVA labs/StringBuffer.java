42a217893a4740361d4bc3d98ce885e318910658
Shanki200801 <shashank200801@gmail.com>
2022-09-23 11:16:52 +0530

    23-11 java labs

---
+++ JAVA labs/StringBuffer.java
@@ -0,0 +1,12 @@
+/**
+ * StringBuffer
+ */
+import java.io.*;
+import java.lang.*;
+public class StringBuffer {
+
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        //sb.append("hellow");
+    }
+}
